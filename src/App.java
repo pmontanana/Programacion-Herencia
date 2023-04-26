0b0b510abff97793fa9a7beac662a472d66380dd
Pablo Montañana Fuentes <pablomontanana@gmail.com>
2023-04-26 18:31:03 +0200

    Ejercicio 13 Hecho

--- src/App.java
+++ src/App.java
@@ -18,10 +18,17 @@ public class App {
     public static void pruebaCajas() {
         Caja test1 = new Caja(20, 20, 20, Unidad.cm);
         System.out.println(test1);
+        System.out.println(String.format("Volumen caja %5.2f ", test1.getVolumen()));
+    }    
+    public static void pruebaCajasCarton() {
+        Caja test1 = new CajaCarton(20, 20, 20);
+        System.out.println(test1);
+        System.out.println(String.format("Volumen caja cartón %5.2f cm3", test1.getVolumen()));
     }
 
     public static void main(String[] args){
         //pruebaInstrumentos();
         pruebaCajas();
+        pruebaCajasCarton();
     }
 }
---
+++ src/CajaCarton.java
@@ -0,0 +1,14 @@
+import Transportes.Caja;
+import Transportes.Unidad;
+
+public class CajaCarton extends Caja{
+
+    public CajaCarton(int ancho, int alto, int fondo) {
+        super(ancho, alto, fondo, Unidad.cm);
+    }
+
+    @Override
+    public double getVolumen() {
+        return super.getVolumen()*.8;
+    }
+}
